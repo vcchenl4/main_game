bf4f2adef4eae44e3a3650b24967798697b07c57
smitches <31672246+smitches@users.noreply.github.com>
2017-09-28 16:29:51 -0500

    Add files via upload

--- state1.js
+++ state1.js
@@ -6,11 +6,10 @@ var state1 = {
     game.load.image('blockP', 'assets/Neon_Block_1_Pink.png');
     game.load.image('blockY', 'assets/Neon Block 1 Yellow.png');
     //game.load.image('platform', 'assets/platform.png');
-    game.load.spritesheet('stickB', 'assets/Stickman Neon Blue sprite_1.png', 200,183,7);
-    game.load.spritesheet('stickB', 'assets/Stickman Neon Blue sprite_1.png', 200,183,7);
-    game.load.spritesheet('stickG', 'assets/Stickman Neon Green sprite_1.png', 200,183,7);
-    game.load.spritesheet('stickP', 'assets/Stickman Neon Pink sprite_1.png', 200,183,7);
-    game.load.spritesheet('stickY', 'assets/Stickman Neon Yellow sprite_1.png', 200,183,7);
+    game.load.spritesheet('stickB', 'assets/Stickman Neon Blue sprite_1.png', 184,183,7);
+    game.load.spritesheet('stickG', 'assets/Stickman Neon Green sprite_1.png', 184,183,7);
+    game.load.spritesheet('stickP', 'assets/Stickman Neon Pink sprite_1.png', 184,183,7);
+    game.load.spritesheet('stickY', 'assets/Stickman Neon Yellow sprite_1.png', 184,183,7);
     
     },
     create: function(){
@@ -32,14 +31,15 @@ var state1 = {
     },
     update:function(){
 		if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-			mc.x += 5;
+			mc.x += 3;
 			mc.scale.setTo(1,1);
-			mc.animations.play('walk',20,true);
+			mc.animations.play('walk',5,true);
+            
         }
 		else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
-			mc.x -= 5;
+			mc.x -= 3;
 			mc.scale.setTo(-1,1);
-			mc.animations.play('walk',20,true);
+			mc.animations.play('walk',5,true);
 		}
 		else{
 			mc.animations.stop()
@@ -49,19 +49,36 @@ var state1 = {
     
 };
 
-function colorChange(i,col){
-	mc.loadTexture('stick'+col);
-	console.log(col)
+function colorChange(keyObject){
+    var color="";
+    
+    if (keyObject.keyCode==Phaser.Keyboard.ONE) {
+        color='B'
+    }
+    else if (keyObject.keyCode==Phaser.Keyboard.TWO) {
+        color='G'
+    }
+    else if (keyObject.keyCode==Phaser.Keyboard.THREE) {
+        color='P'
+    }
+    else if (keyObject.keyCode==Phaser.Keyboard.FOUR) {
+        color='Y'
+    }
+    
+	mc.loadTexture('stick'+color);
+    mc.animations.add('walk',[0,1,2,3,4,5,6,7]);
+    console.log(color);
 }
 
 function addKeyCallback(key,func,args){
 	game.input.keyboard.addKey(key).onDown.add(func,null,null,args);
-	console.log(args);
+	
 }
 
 function addChangeEventListener(){
 	addKeyCallback(Phaser.Keyboard.ONE,colorChange,'P');
 	addKeyCallback(Phaser.Keyboard.TWO,colorChange,'Y');
 	addKeyCallback(Phaser.Keyboard.THREE,colorChange,'B');
-	addKeyCallback(Phaser.Keyboard.FOUR,colorChange,'G');	
+	addKeyCallback(Phaser.Keyboard.FOUR,colorChange,'G');
+    
 }
