710e7ee94c348adcd684517dea297998bcbdb96c
Brian Smith-Eitches <beadsdd@wireless-10-145-201-240.public.utexas.edu>
2017-10-01 18:26:53 -0500

    added passsthrough

--- state1.js
+++ state1.js
@@ -1,4 +1,6 @@
-var mc = {} ,blockB, blockG, blockP, blockY, platforms;
+var mc = {} , blockB, blockG, blockP, blockY, platforms, guycolor, block1,block2, block3, block4;
+
+
 var width = 2000 
 var bottom = 1000
 var state1 = {
@@ -10,14 +12,14 @@ var state1 = {
 		game.load.spritesheet('stickY', 'assets/Stickman Neon Yellow sprite_2.png', 160,180,8);
 	
 		//enviroment preloading
-		game.load.image('bg','assets/background_final.jpg')
+		
 		game.load.image('ground','assets/ground.png');
 		game.load.image('testwall','assets/testwall.png');
-		/*
+		
 		game.load.image('blockB', 'assets/Neon Block 1 Blue.png');
 		game.load.image('blockG', 'assets/Neon Block 1 Green.png');
 		game.load.image('blockP', 'assets/Neon_Block_1_Pink.png');
-		game.load.image('blockY', 'assets/Neon Block 1 Yellow.png');*/
+		game.load.image('blockY', 'assets/Neon Block 1 Yellow.png');
     
     },
     create: function(){
@@ -25,31 +27,56 @@ var state1 = {
 		//game.add.image(0,0,'bg');
         game.physics.startSystem(Phaser.Physics.ARCADE);
 		//game.world.setBounds(0,0,1000,1000);
+		blockB= game.add.group();
+        blockY= game.add.group();
+        blockG= game.add.group();
+        blockP= game.add.group();
+        blockB.enableBody=true;
+        blockY.enableBody=true;
+        blockG.enableBody=true;
+        blockP.enableBody=true;
+        
 		
-		/*
-		blockB = game.add.sprite(500,500,'blockB');
-		blockY = game.add.sprite(800,500,'blockY');
-		blockP = game.add.sprite(500,800,'blockP');
-		blockG = game.add.sprite(800,800,'blockG');*/
+		block1 = blockB.create(1000,800,'blockB');
+        block1.body.immovable=true;
+        block1.anchor.x=.5;
+        block1.anchor.y=.5;
+        
+		block2 = blockY.create(800,800,'blockY');
+        block2.anchor.x=.5;
+        block2.anchor.y=.5
+		block2.body.immovable=true;
+        
+        block3 = blockP.create(500,800,'blockP');
+        block3.anchor.x=.5;
+        block3.anchor.y=.5
+		block3.body.immovable=true;
+        
+        block4 = blockG.create(275,800,'blockG');
+        block4.anchor.x=.5;
+        block4.anchor.y=.5;
+        block4.body.immovable=true;
         
 		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		mc = game.add.sprite(0,300,'stickP');
+        guycolor='P';
 		mc.animations.add('walk',[0,1,2,3,4,5,6,7,6,5,4,3,2,]);
         platforms = game.add.group();
         platforms.enableBody = true;
         
-        var ground = platforms.create(0, bottom-20, 'ground');
-		var test_wall = platforms.create(700,0,'testwall');
+        var ground = platforms.create(0, bottom-200, 'ground');
+		//var test_wall = platforms.create(700,0,'testwall');
+        //test_wall.body.immovable = true;
+		//test_wall.scale.setTo(1,10);
+        
         
         ground.body.immovable = true;
 		//for now we have complete ground coverage, we can change this later in the builds
 		// ground width = 368 + height = 21
         ground.scale.setTo((width /368),1);
-		test_wall.body.immovable = true;
-		test_wall.scale.setTo(1,10);
-        
+		
         mc.anchor.x=.5;
-        mc.anchor.y= 1;
+        mc.anchor.y= .5;
 		addChangeEventListener();
         game.physics.arcade.enable(mc);
         mc.body.collideWorldBounds=true;
@@ -58,8 +85,10 @@ var state1 = {
     },
     update:function(){
         game.physics.arcade.collide(mc, platforms);
-		addMoveEventListener()
-		walltouch()
+        passthrough();
+		addMoveEventListener();
+		walltouch();
+        passthrough();
 	
 		game.input.keyboard.onUpCallback = function(){
 			mc.body.velocity.x = 0;
@@ -98,23 +127,25 @@ function addKeyCallback(key,func,args){
 }
 
 function colorChange(keyObject){
-    var color = "";
+    guycolor=""
     if (keyObject.keyCode==Phaser.Keyboard.ONE) {
-        color='B'
+        guycolor='B';
+        
     }
     else if (keyObject.keyCode==Phaser.Keyboard.TWO) {
-        color='G'
+        guycolor='G';
     }
     else if (keyObject.keyCode==Phaser.Keyboard.THREE) {
-        color='P'
+        guycolor='P';
     }
     else if (keyObject.keyCode==Phaser.Keyboard.FOUR) {
-        color='Y'
+        guycolor='Y';
     }
     
-	mc.loadTexture('stick'+color);
+	mc.loadTexture('stick'+guycolor);
     mc.animations.add('walk',[0,1,2,3,4,5,6,7,6,5,4,3,2]);
-    console.log(color);
+    console.log(guycolor);
+    
 	}
 
 function addChangeEventListener(){
@@ -147,11 +178,26 @@ function moving(keypress){
 	}
 }
 
+function passthrough(){
+    if (guycolor!="P"){
+        game.physics.arcade.collide(mc, blockP);
+    }
+    if (guycolor!="G"){
+        game.physics.arcade.collide(mc, blockG);
+    }
+    if (guycolor!="Y"){
+        game.physics.arcade.collide(mc, blockY);
+    }
+    if (guycolor!="B"){
+        game.physics.arcade.collide(mc, blockB);
+    }
+};
+
 function walltouch(){
 	if (mc.body.touching.down == false){
 		if (mc.body.touching.right == true || mc.body.touching.left == true){
 			console.log(mc.body.touching.right)
-			mc.body.velocity.y = 20;
+			mc.body.velocity.y = -50;
 			mc.body.gravity.y = 0
 		}
 		else{
