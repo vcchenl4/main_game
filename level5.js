b0fd99745703c1ce474d4f8be80ce44817fb85b2
Henry  Gammad <henrygammad@Henrys-MacBook-Pro.local>
2017-11-12 12:28:54 -0600

    here are my changes 2

--- level5.js
+++ level5.js
@@ -22,34 +22,50 @@ var level5 = {
 		//creates the ground
         ground=createPlatform(0,bottom + 280,'ground',1000,1);
 		//makes a ground
-		ground2=createPlatform(250,800,'ground',.315,16.5);
-		
+		ground2=createPlatform(25,350,'ground',1.4,2.2);
+		blackblock=createPlatform(350,800,'ground',.315,16.5);
+		blackblock2=createPlatform(350,935,'ground',.315,16.5);
         
         ///////////////////All Level 3 code/////////////////////////
 		
 		
-		createBlueCollide(25,800,'wallB',1,1);
-		createPinkCollide(250,935,'wallP',1,1);
-		createPinkCollide(750,935,'wallP',1,1);
-		createPinkCollide(1050,935,'wallP',1,1);
-		
-		
+		createYellowCollide(25,400,'wallY',1,1);
+		createYellowCollide(145,400,'wallY',1,1);
+		createYellowCollide(265,400,'wallY',1,1);
 		createGreenCollide(425,400,'wallG',1,1);
-		createPinkCollide(425,750,'wallP',1,1);
+		createGreenCollide(545,400,'wallG',1,1);
+		
+		createPinkCollide(1110,935,'wallP',1,1);
+		createPinkCollide(750,935,'wallP',1,1);
+		createPinkCollide(870,935,'wallP',1,1);
+		createPinkCollide(990,935,'wallP',1,1);
+		
+		createGreenCollide(750,585,'wallG',1,1);
+		createBlueCollide(870,585,'wallB',1,1);
+		createBlueCollide(990,585,'wallB',1,1);
+		createGreenCollide(1110,585,'wallG',1,1);
+
+		//(120 x differnce and 350 y differnce
+		createYellowCollide(1230,585,'wallY',1,1);
+		createYellowCollide(1230,935,'wallY',1,1);
+		createGreenCollide(1110,585,'wallG',1,1);
+		createBlueCollide(990,585,'wallB',1,1);
 
 		
-		createBlueCollide(750,400,'wallB',1,1);
+		
 
-		createBlueCollide(1050,400,'wallB',1,1);
 
 		
-		
-		
+		//createBlueCollide(750,400,'wallB',1,1);
+		//createBlueCollide(1050,400,'wallB',1,1);
+
 		
         //add in the MC to the game
 		mc = game.add.sprite(10,(bottom + 920) - 955 * .6  - 150,'stickB');
         guycolor='B';
-		mc.animations.add('walk',[0,1,2,3,4,5,6,7,6,5,4,3,2,]);
+		mc.animations.add('jump',[8]);
+		mc.animations.add('slide',[9]);
+		mc.animations.add('walk',[0,1,2,3,4,5,6,7,6,5,4,3,2,])
         mc.anchor.x=.5;
         mc.anchor.y= .5;
 		game.physics.arcade.enable(mc);
