85e33723de00c9f987d86a098394825663cf0a82
Victor Chen <vc8724@utexas.edu>
2017-11-19 15:02:16 -0600

    finished the main menu state

--- mainMenu.js
+++ mainMenu.js
@@ -1,38 +1,21 @@
 var mainMenu = {
 	preload: function(){
-		preloadall();
-		
-		
+		//loading main menu image
+		game.load.image("mainmenu","assets/mainmenu_copy.png")
+		game.load.spritesheet("play","assets/play button.png",550,225)
 	},
-	
-	
-	create: function(){
-		createrules(1,1,"mainMenu");
-		game.world.setBounds(0,0,width,bottom);
-		//Create the player
-		mc = game.add.sprite(0,(bottom + 280) - 955 * .6  - 150,'stickB');
-        guycolor='B';
-		mc.animations.add('jump',[8]);
-		mc.animations.add('slide',[9]);
-		mc.animations.add('walk',[0,1,2,3,4,5,6,7,6,5,4,3,2,]);
-        mc.anchor.x=.5;
-        mc.anchor.y= .5;
-		game.physics.arcade.enable(mc);
-        mc.body.collideWorldBounds=true;
-        game.camera.follow(mc)
-		mc.body.gravity.y = 400
-		//create the ground
-		ground=createPlatform(0,990,'ground',1000,1);
-		wall=createPlatform(1980,0,'ground',10,100);
-		wall2=createPlatform(-173,0,'ground',.5,100);
 
+	create: function(){
+		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		game.add.sprite(0,0,"mainmenu");
+		//game.add.sprite(width/3 + 75,600,"play");
+		main = game.add.button(width/3 + 75,600,"play",enterTutorial,1,1,0,1);
 	},
 	
 	update: function(){
-		updateall();
-		
-		
 	}
-	
-	
+}
+
+function enterTutorial(){
+	game.state.start('tutorialstate');
 }
